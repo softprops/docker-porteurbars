24e9732aa3961f38c24d0855387a57a3b51b3c76
softprops <d.tangren@gmail.com>
2014-10-24 08:51:08 -0400

    parse{Image,Port} helpers

--- src/main/scala/Helpers.scala
+++ src/main/scala/Helpers.scala
@@ -13,6 +13,54 @@ trait DockerHelpers {
   def truncateId(id: String): String =
     id.take(12)
 
+  def stripSlash(str: String): String =
+    str.replaceFirst("/", "")
+
+  /** parses port into components @port and @type (tcp or udp) */
+  def parsePort(str: String, options: Options): CharSequence = {
+    val sb = new StringBuilder()
+    if (str.isEmpty) sb.append(options.inverse()) else {
+      val parent = options.context
+      val ctxBuilder = Context.newBuilder(parent, str)
+      val (port, typ) = str.split("/", 2) match {
+        case Array(port, typ) => (port, typ)
+        case Array(port) => (port, "tcp")
+      }
+      val ctx = ctxBuilder
+        .combine("@port", port)
+        .combine("@type", typ)
+        .build()
+      sb.append(options(options.fn, ctx))
+      ctx.destroy()
+    }
+    sb
+  }
+
+  /** parses docker image string into @registry @repo and @tag components */
+  def parseImage(str: String, options: Options): CharSequence = {
+    val sb = new StringBuilder()
+    if (str.isEmpty) sb.append(options.inverse()) else {
+      val parent = options.context
+      val ctxBuilder = Context.newBuilder(parent, str)
+      val (reg, rest) = str.split("/", 2) match {
+        case Array(reg, rest) => (reg, rest)
+        case Array(rest) => ("", rest)
+      }
+      val (repo, tag) = rest.split(":", 2) match {
+        case Array(repo, tag) => (repo, tag)
+        case Array(repo) => (repo, "latest")
+      }
+      val ctx  = ctxBuilder
+        .combine("@registry", reg)
+        .combine("@repo", repo)
+        .combine("@tag", tag)
+        .build()
+      sb.append(options(options.fn, ctx))
+      ctx.destroy()
+    }
+    sb
+  }
+
   def kv(combined: String, options: Options) = keyvalue(combined, options)
 
   def keyvalue(combined: String, options: Options): CharSequence = {
