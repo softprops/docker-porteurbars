44b987654285892b89f1f5e74b66e30b35e6634f
softprops <d.tangren@gmail.com>
2014-10-26 11:13:34 -0400

    better organization

--- src/main/scala/Template.scala
+++ src/main/scala/Template.scala
@@ -4,21 +4,17 @@ import com.github.jknack.handlebars.{
   Context, Handlebars, Template => HandlebarsTemplate, ValueResolver
 }
 import com.github.jknack.handlebars.io.FileTemplateLoader
-import tugboat.Docker
 import org.json4s.{ JArray, JValue }
 import org.json4s.native.JsonMethods._
 import scala.concurrent.{ Future, ExecutionContext }
+import tugboat.Docker
 
 object Template {
-  val resolvers: Array[ValueResolver] =
-    Array(Json4sResolver, ScalaResolver) ++ ValueResolver.VALUE_RESOLVERS
-  def newContext(obj: Object) =
-    Context.newBuilder(obj)
-      .combine("Env", sys.env)
-      .resolver(resolvers:_*)      
-      .build()
   val compiler: Handlebars =
     new Handlebars(new FileTemplateLoader("")).registerHelpers(Helpers)
+
+  val resolvers: Array[ValueResolver] =
+    Array(Json4sResolver, ScalaResolver) ++ ValueResolver.VALUE_RESOLVERS
 
   def apply[T: Input]
    (in: T)
@@ -29,6 +25,12 @@ object Template {
    (in: T, docker: Docker)
    (implicit ec: ExecutionContext): Template =
     Template(implicitly[Input[T]].contents(in), docker)
+
+  def newContext(obj: Object) =
+    Context.newBuilder(obj)
+      .combine("Env", sys.env)
+      .resolver(resolvers:_*)
+      .build()
 }
 
 case class Template
@@ -43,9 +45,6 @@ case class Template
   private[this] lazy val inspect =
     docker.containers.get(_)
 
-  def configure(update: Handlebars => Handlebars) =
-    copy(compiler = update(compiler))
-
   def apply(): Future[String] = {
     containers().flatMap { up =>
       Future.sequence(up.map { c =>
@@ -53,6 +52,9 @@ case class Template
       })
     }.map(JArray(_)).map(render)
   }
+
+  def configure(update: Handlebars => Handlebars) =
+    copy(compiler = update(compiler))
 
   def close() = docker.close()
 
